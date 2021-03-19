4da17fd298f211abcd1473694d0bd78c9d8253cd
vladyslavs <vladyslav.snozyk@superfund.com>
2021-03-19 10:19:53 +0100

    option map2

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -27,4 +27,18 @@ sealed trait Option[+A] {
 
 case class Some[+A](get:A) extends Option[A]
 case object None extends Option[Nothing]
+object Option {
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C]={
+    a.flatMap(x=> b.map(y=>f(x,y)))
+  }
+  def sequence[A](a: List[Option[A]]): Option[List[A]]={
+    List.foldLeft(a,Some(Nil):Option[List[A]])((acc, el)=>map2(acc,el)(List.appendLeft))
+  }
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]]={
+    a match {
+      case Nil => Some(Nil)
+      case Cons(h,t)=> map2(f(h), traverse(t)(f))(Cons(_,_))
+    }
+  }
 
+}
